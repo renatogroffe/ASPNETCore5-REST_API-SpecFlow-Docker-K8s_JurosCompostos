cc4977463c87b6bcf82c2f1306a0b07e4f594ea3
Renato Groffe <renato.groffe@yahoo.com.br>
2021-05-24 19:38:51 -0300

    .NET 5 + ASP.NET Core + REST API + xUnit + SpecFlow + Fluent Assertions + Swagger + Dockerfile + Azure DevOps + Azure Kubernetes Service + Juros Compostos
    .NET 5 + ASP.NET Core + REST API + xUnit + SpecFlow + Fluent Assertions + Swagger + Dockerfile + Azure DevOps + Azure Kubernetes Service + Juros Compostos

---
+++ APIFinancas.Especificacoes/CalculoJurosCompostosStepDefinition.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+
+namespace APIFinancas.Especificacoes
+{
+    [Binding]
+    public class CalculoJurosCompostosStepDefinition
+    {
+        private double _valorEmprestimo;
+        private int _numMeses;
+        private double _percTaxa;
+        private double _valorCalculado;
+
+        [Given(@"que o valor o valor do empréstimo é de R\$ (.*)")]
+        public void PreencherValorEmprestimo(double valorEmprestimo)
+        {
+            _valorEmprestimo = valorEmprestimo;
+        }
+
+        [Given(@"que este empréstimo será por (.*) meses")]
+        public void PreencherNumeroMeses(int numMeses)
+        {
+            _numMeses = numMeses;
+        }
+
+        [Given(@"que a taxa de juros é de (.*)% ao mês")]
+        public void PreencherPercentualTaxa(double percTaxa)
+        {
+            _percTaxa = percTaxa;
+        }
+
+        [When(@"eu solicitar o cálculo do valor total a ser pago ao final do período")]
+        public void ProcessarCalculoJurosCompostos()
+        {
+            _valorCalculado = CalculoFinanceiro
+                .CalcularValorComJurosCompostos(
+                    _valorEmprestimo, _numMeses, _percTaxa);
+        }
+
+        [Then(@"o resultado será (.*)")]
+        public void ValidarResultado(double valorFinalEmprestimo)
+        {
+            _valorCalculado.Should().Be(valorFinalEmprestimo,
+                " * diferenca no calculo |" +
+                $" valorEmprestimo: {_valorEmprestimo} |" +
+                $" numMeses: {_numMeses} |" +
+                $" percTaxa: {_percTaxa} *");
+            // Implementação equivalente com Assert:
+            // Assert.Equal(valorFinalEmprestimo, _valorCalculado);
+        }
+    }
+}
